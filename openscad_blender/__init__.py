4a3fdac9b543e23391ca0f78cc957983d86a1256
Dwight Hubbard <d@dhub.me>
2014-05-30 00:19:42 -0700

    Significant work to switch to using the Freecad python apis.

--- openscad_blender/__init__.py
+++
@@ -1,8 +0,0 @@
-__author__ = 'dwight'
-# Blender measurements are two large, and when blender is set to metric measurements it uses meters as the value of
-# 1 not mm like openscad.  Thiss value is the translation factor to use
-MEASUREMENT_DIVISOR = 1000
-
-from collections import defaultdict
-object_index = 0
-objects_index = defaultdict(lambda:0)
--- openscad_blender/functions/other.py
+++
@@ -1,6 +0,0 @@
-from __future__ import print_function
-__author__ = 'dwight'
-
-
-def echo(*args):
-    print(*args)
